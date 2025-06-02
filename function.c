8f39cd7c58b701d45cb82e8e911cc79e9e546a57
Name=Maomy Vincent <maomy@rh8-lfs-09.linsee.dyn.nesc.nokia.net>
2025-06-02 15:09:51 +0300

    second commit

--- function.c
+++ function.c
@@ -4,6 +4,6 @@ void calcul(int a, int b){
 	printf("le resultat de l'operation a+b est %d\n",a+b);
 	printf("le resultat de l'operation a-b est %d\n",a-b);
 	printf("le resultat de l'operation a*b est %d\n",a*b);
-	if(b!=0) printf("le resultat de l'operation a/b est %d\n",a/b);
+	if(b!=0) printf("le resultat de l'operation a/b est %2.2f\n",a/(float)b);
 	else puts("a/b n'est pas possible : Erreur division par 0!");
 }
