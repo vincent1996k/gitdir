585b4ff5b4f759a78bb2f329b9db6838edd8fc75
Name=Maomy Vincent <maomy@rh8-lfs-09.linsee.dyn.nesc.nokia.net>
2025-06-02 18:23:19 +0300

    seventh commit

--- c/function.c
+++ c/function.c
@@ -6,12 +6,12 @@ void calcul(int a, int b){
 	printf("le resultat de l'operation a*b est %d\n",a*b);
 	if(b!=0) printf("le resultat de l'operation a/b est %2.2f\n",a/(float)b);
 	else puts("a/b n'est pas possible : Erreur division par 0!");
-	printf("Le resultat de l'operation !a est %d\n",factoriel(a));
-	printf("Le resultat de l'operation !b est %d\n",factoriel(b));
+	printf("Le resultat de l'operation a! est %ld\n",factoriel(a));
+	printf("Le resultat de l'operation b! est %ld\n",factoriel(b));
 
 }
 
-int factoriel(int n){
+long factoriel(int n){
 	if(n==0) return 1;
         else return n*factoriel(n-1);
 }
