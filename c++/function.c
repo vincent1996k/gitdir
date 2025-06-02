937ec07a9c14cfde843a19f1e5f91ec15dd04520
Name=Maomy Vincent <maomy@rh8-lfs-09.linsee.dyn.nesc.nokia.net>
2025-06-02 16:15:50 +0300

    fifth commit

---
+++ c++/function.c
@@ -0,0 +1,17 @@
+#include "function.h"
+
+void calcul(int a, int b){
+	cout<<"le resultat de l'operation a+b est "<<a+b<<endl;
+	cout<<"le resultat de l'operation a-b est "<<a-b<<endl;
+	cout<<"le resultat de l'operation a*b est "<<a*b<<endl;
+	if(b!=0) cout<<"le resultat de l'operation a/b est "<<a/(float)b<<endl;
+	else cout<<"a/b n'est pas possible : Erreur division par 0!"<<endl;
+	cout<<"Le resultat de l'operation a! est "<<factoriel(a)<<endl;
+	cout<<"Le resultat de l'operation b! est "<<factoriel(b)<<endl;
+
+}
+
+int factoriel(int n){
+	if(n==0) return 1;
+    else return n*factoriel(n-1);
+}
